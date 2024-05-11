3927cca1adc2d3cd18b88bf9d489c78ae1469ce3
Kseniia Petrushina <petrushina.ke@phystech.edu>
2024-05-11 17:58:22 +0300

    update learning

--- learning.py
+++ learning.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 class Learner:
-    def __init__(self, W, A_d=1, A_l=1, tau_d=1, tau_l=1, a_d=0, a_l=0):
-        self.W = W
+    def __init__(self,A_d=1, A_l=1, tau_d=1, tau_l=1, a_d=0, a_l=0):
         self.A_d = A_d
         self.A_l = A_l
         self.tau_d = tau_d
@@ -16,8 +15,25 @@ class Learner:
     def W_l(self, s_l):
         return np.where(s_l > 0, - self.A_l * np.exp(-s_l / self.tau_l), 0)
 
-    def update(self, t, S_l, S_d, S_in, times=None):
-        # S_d: [n_neurons, times]
-        new_times = np.tile(self.W_d(times), (4, 1))
-        dw = (S_d[:, t] - S_l[:, t]) * (self.a_d + np.where(S_in, new_times, 0))
-        return dw
+
+    def update(self, t, S_l, S_d, S_in, dt=1):
+        # S_l [num_ts], S_d [num_ts], S_in [num_neurons, num_ts]
+
+        num_neurons, num_ts = S_in.shape
+        time_id = int(t / dt)
+
+        dws = []
+        for neuron in range(num_neurons):
+            dw_d = dw_l = 0
+
+            for j in range(len(S_d)):
+                s_d = t - 0.001 * dt * j
+                dw_d += self.W_d(s_d) * S_in[neuron, j]
+            
+            for j in range(len(S_l)):
+                s_l = t - 0.001 * dt * j
+                dw_l += self.W_l(s_l) * S_in[neuron, j]
+
+            dws.append(S_d[time_id] * (self.a_d + dw_d) + S_l[time_id] * (self.a_l + dw_l))
+
+        return dws
